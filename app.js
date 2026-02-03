9d5f6f2f7488115565a3685704d5e29f6935fb10
Noor AlHadidi <noor.ahmed.alhadidi@gmail.com>
2026-02-03 16:52:19 +0200

    imported router in app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import todosRouter from "./routes/todos.routes.js";
+
 dotenv.config();
 
 const app = express();
@@ -12,3 +14,5 @@ const PORT = Number(process.env.PORT) || 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
+app.use("/todos", todosRouter);

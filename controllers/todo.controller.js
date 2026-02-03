41e64d388a7ced8340fd7d6a9604ed1eead541a4
Noor AlHadidi <noor.ahmed.alhadidi@gmail.com>
2026-02-04 00:49:35 +0200

    added post and patch requests

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,30 +1,59 @@
-import { getAllTodos } from "../services/todo.service.js";
+import { saveTodo, getSingleTodo, getAllTodos, modifyTodo, changeTodo } from "../services/todo.service.js";
 
-export function addTodo(req, res) {
-  return res.status(201).json({ message: "Adding To-Do" });
+export async function addTodo(req, res) {
+    try {
+        const { text } = req.body;
+        const newTodo = await saveTodo(text);
+        return res.status(201).json(newTodo);
+    } catch (error) {
+        return res.status(500).json({ message: "Failed to add todo." });
+    }
 }
 
-export function getTodo(req, res) {
-  return res.status(200).json({ message: "To-Do" });
+export async function getTodo(req, res) {
+    try {
+        const id = Number(req.params.id);
+        const todo = await getSingleTodo(id);
+        if (!todo) {
+            return res.status(404).json({ message: "No todo with that ID exists." })
+        }
+        return res.status(200).json(todo);
+    } catch (error) {
+        return res.status(500).json({ message: "Failed to get todo." });
+    }
 }
 
 export async function getTodos(req, res) {
-  try {
-    const todos = await getAllTodos();
-    res.status(200).json(todos);
-  } catch (error) {
-    res.status(500).json({ message: "Failed to get todos." });
-  }
+    try {
+        const todos = await getAllTodos();
+        res.status(200).json(todos);
+    } catch (error) {
+        res.status(500).json({ message: "Failed to get todos." });
+    }
 }
 
-export function updateTodo(req, res) {
-  return res.status(200).json({ message: "Updating To-Do" });
+export async function updateTodo(req, res) {
+    try {
+        const id = Number(req.params.id);
+        const { text } = req.body;ß
+        await modifyTodo(id, text);
+        return res.sendStatus(200)
+    } catch (error) {
+        console.error("Error updating todo:", error);
+        return res.status(500).json({ message: "Failed to update todo." });    
+    }
 }
 
-export function markTodo(req, res) {
-  return res.status(200).json({ message: "Marking To-Do" });
+export async function markTodo(req, res) {
+    try {
+        const id = Number(req.params.id);
+        await changeTodo(id);
+        return res.sendStatus(200)
+    } catch (error) {
+        return res.status(500).json({ message: "Failed to mark todo." });    
+    }
 }   
 
 export function deleteTodo(req, res) {
-  return res.status(204).json({ message: "Deleting To-Do" });
+    return res.status(204).json({ message: "Deleting To-Do" });
 }
--- services/todo.service.js
+++ services/todo.service.js
@@ -2,8 +2,41 @@ import fs from "fs/promises";
 
 const DATA_PATH = "./data/todos.json";
 
+export async function saveTodo(text) {
+    const todos = await getAllTodos();
+    const newId = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1;
+    const newTodo = {
+        id: newId,
+        text: text,
+        done: false,
+        createdAt: new Date().toISOString(),
+  };
+  todos.push(newTodo);
+  await fs.writeFile(DATA_PATH, JSON.stringify(todos, null, 2));
+  return newTodo;
+}
+
+export async function getSingleTodo(id) {
+    const todos = await getAllTodos();
+    return todos.find((todo) => todo.id === id);
+}
+
 export async function getAllTodos() {
-  const data = await fs.readFile(DATA_PATH, "utf-8");
-  const todos = JSON.parse(data);
-  return todos;
+    const data = await fs.readFile(DATA_PATH, "utf-8");
+    const todos = JSON.parse(data);
+    return todos;
+}
+
+export async function modifyTodo(id, newText) {
+    const todos = await getAllTodos();
+    const todo = todos.find((todo) => todo.id === id);
+    todo.text = newText;
+    await fs.writeFile(DATA_PATH, JSON.stringify(todos, null, 2));
+}
+
+export async function changeTodo(id) {
+    const todos = await getAllTodos();
+    const todo = todos.find((todo) => todo.id === id);
+    todo.done = !todo.done;
+    await fs.writeFile(DATA_PATH, JSON.stringify(todos, null, 2));
 }

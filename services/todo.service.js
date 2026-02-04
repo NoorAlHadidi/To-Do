45bad9a9a66ae14b2a4a3b4bb65502cb37317525
Noor AlHadidi <noor.ahmed.alhadidi@gmail.com>
2026-02-04 13:15:37 +0200

    added save todo functionality

--- services/todo.service.js
+++ services/todo.service.js
@@ -1,65 +1,28 @@
-import fs from "fs/promises";
-
-const DATA_PATH = "./data/todos.json";
+import connection from "../config.js";
 
 export async function saveTodo(text) {
-    const todos = await getAllTodos();
-    const newId = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1;
-    const newTodo = {
-        id: newId,
-        text: text,
-        done: false,
-        createdAt: new Date().toISOString(),
-  };
-  todos.push(newTodo);
-  await fs.writeFile(DATA_PATH, JSON.stringify(todos, null, 2));
-  return newTodo;
+    const query = "INSERT INTO todo_items (text) VALUES ($1) RETURNING *;";
+    const values = [text];
+    const result = await connection.query(query, values);
+    return {
+        id: result.rows[0].id,
+        text: result.rows[0].text,
+        done: result.rows[0].done,
+        createdAt: result.rows[0].created_at
+    };
 }
 
 export async function getSingleTodo(id) {
-    const todos = await getAllTodos();
-    const todo = todos.find((todo) => todo.id === id);
-    if (!todo) {
-        return null;
-    }
-    return todo;
 }
 
 export async function getAllTodos() {
-    const data = await fs.readFile(DATA_PATH, "utf-8");
-    const todos = JSON.parse(data);
-    return todos;
 }
 
 export async function modifyTodo(id, newText) {
-    const todos = await getAllTodos();
-    const todo = todos.find((todo) => todo.id === id);
-    if (!todo) {
-        return null;
-    }
-    todo.text = newText;
-    await fs.writeFile(DATA_PATH, JSON.stringify(todos, null, 2));
-    return todo;
 }
 
 export async function changeTodo(id) {
-    const todos = await getAllTodos();
-    const todo = todos.find((todo) => todo.id === id);
-    if (!todo) {
-        return null;
-    }
-    todo.done = !todo.done;
-    await fs.writeFile(DATA_PATH, JSON.stringify(todos, null, 2));
-    return todo;
 }
 
 export async function removeTodo(id) {
-    const todos = await getAllTodos();
-    const deletedTodo = todos.find((todo) => todo.id === id);
-    if (!deletedTodo) {
-        return null;
-    }
-    const filteredTodos = todos.filter((todo) => todo.id !== id);
-    await fs.writeFile(DATA_PATH, JSON.stringify(filteredTodos, null, 2));
-    return deletedTodo;
 }

6bc67f471af1ed623695b91c33c29e30fcd3f08f
Noor AlHadidi <noor.ahmed.alhadidi@gmail.com>
2026-02-04 17:15:55 +0200

    implemented CRUD operations

--- services/todo.service.js
+++ services/todo.service.js
@@ -1,65 +1,43 @@
-import fs from "fs/promises";
-
-const DATA_PATH = "./data/todos.json";
+import database from '../database/config.js';
 
 export async function saveTodo(text) {
-    const todos = await getAllTodos();
-    const newId = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1;
-    const newTodo = {
-        id: newId,
-        text: text,
-        done: false,
-        createdAt: new Date().toISOString(),
-  };
-  todos.push(newTodo);
-  await fs.writeFile(DATA_PATH, JSON.stringify(todos, null, 2));
-  return newTodo;
+    const todo = await database('todo_knex').insert({ title: text }).returning('*');
+    return todo[0];
 }
 
 export async function getSingleTodo(id) {
-    const todos = await getAllTodos();
-    const todo = todos.find((todo) => todo.id === id);
-    if (!todo) {
+    const todo = await database('todo_knex').where({ id });
+    if (todo.length === 0) {
         return null;
     }
-    return todo;
+    return todo[0];
 }
 
 export async function getAllTodos() {
-    const data = await fs.readFile(DATA_PATH, "utf-8");
-    const todos = JSON.parse(data);
+    const todos = await database('todo_knex').select('*');
     return todos;
 }
 
 export async function modifyTodo(id, newText) {
-    const todos = await getAllTodos();
-    const todo = todos.find((todo) => todo.id === id);
-    if (!todo) {
+    const todo = await database('todo_knex').where({ id }).update({ title: newText }).returning('*');
+    if (todo.length === 0) {
         return null;
     }
-    todo.text = newText;
-    await fs.writeFile(DATA_PATH, JSON.stringify(todos, null, 2));
-    return todo;
+    return todo[0];
 }
 
 export async function changeTodo(id) {
-    const todos = await getAllTodos();
-    const todo = todos.find((todo) => todo.id === id);
-    if (!todo) {
+    const todo = await database('todo_knex').where({ id }).update({done: database.raw('NOT done')}).returning('*');
+    if (todo.length === 0) {
         return null;
     }
-    todo.done = !todo.done;
-    await fs.writeFile(DATA_PATH, JSON.stringify(todos, null, 2));
-    return todo;
+    return todo[0];
 }
 
 export async function removeTodo(id) {
-    const todos = await getAllTodos();
-    const deletedTodo = todos.find((todo) => todo.id === id);
-    if (!deletedTodo) {
+    const todo = await database('todo_knex').where({ id }).del().returning('*');
+    if (todo.length === 0) {
         return null;
     }
-    const filteredTodos = todos.filter((todo) => todo.id !== id);
-    await fs.writeFile(DATA_PATH, JSON.stringify(filteredTodos, null, 2));
-    return deletedTodo;
+    return todo[0];
 }
